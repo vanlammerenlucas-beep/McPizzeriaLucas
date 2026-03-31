66763114aa4a0235483fa108ec24211640b03236
Lucas <vanlammerenlucas@gmail.com>
2026-03-31 09:28:03 +0200

    Gemaakt tot Gegevens uit een tabel verwijderen

--- MCPizzeriaSQL.py
+++ MCPizzeriaSQL.py
@@ -15,7 +15,40 @@ with sqlite3.connect("MCPizzeria.db") as db:
 
 
 ### ---------  Functie definities  -----------------
+def printTabel(tabel_naam):
+    cursor.execute("SELECT * FROM " + tabel_naam) #SQL om ALLE gegevens te halen
+    opgehaalde_gegevens = cursor.fetchall() #sla gegevens op in een variabele
+    print("Tabel " + tabel_naam + ":", opgehaalde_gegevens) #druk gegevens af
+
+def maakTabellenAan():
+ # Maak een nieuwe tabel met 3 kolommen: id, naam, prijs
+ cursor.execute("""
+ CREATE TABLE IF NOT EXISTS tbl_pizzas(
+ gerechtID INTEGER PRIMARY KEY AUTOINCREMENT,
+ gerechtNaam TEXT NOT NULL,
+ gerechtPrijs REAL NOT NULL);""")
+ print("Tabel 'tbl_pizzas' aangemaakt.")
+
+def voegPizzaToe(naam_nieuwe_pizza, prijs_nieuwe_pizza):
+    cursor.execute("INSERT INTO tbl_pizzas VALUES(NULL, ?, ? )", (naam_nieuwe_pizza, prijs_nieuwe_pizza))
+    db.commit() #gegevens naar de database wegschrijven
+    print("Pizzas toegevoegd:")
+    printTabel("tbl_pizzas")
+
+def verwijderPizza(gerechtNaam):
+   cursor.execute("DELETE FROM tbl_pizzas WHERE gerechtNaam = ?", (gerechtNaam,))
+   print("Gerecht verwijderd uit 'tbl_pizzas':", gerechtNaam )
+   db.commit() #gegevens naar de database wegschrijven
+   printTabel("tbl_pizzas")
+
+
+
 
 
 ### --------- Hoofdprogramma  ---------------
-
+maakTabellenAan()
+printTabel("tbl_pizzas")
+voegPizzaToe("Margarita", 9.5)
+voegPizzaToe("Hawaii", 12.25)
+voegPizzaToe("Salami", 10.0)
+verwijderPizza("Hawaii")
